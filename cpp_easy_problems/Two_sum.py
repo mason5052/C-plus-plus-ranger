57f11bead8291821fce95401e80fcd400ec4a7d0
KIM JUNKUK <54308434+Kimjunkuk@users.noreply.github.com>
2021-08-02 21:13:07 -0600

    Create Two_sum.py

---
+++ cpp_easy_problems/Two_sum.py
@@ -0,0 +1,11 @@
+# Py3 base code
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+
+# [Py3][Mason][Pass]
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        for i in range(len(nums)):
+            for j in range(i + 1, len(nums)):
+                if nums[j] == target - nums[i]:
+                    return [i, j]
